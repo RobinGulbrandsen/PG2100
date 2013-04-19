ea18a2f17bc2f09f74d2634d4f353fd5778dd3f3
Robin Gulbrandsen <robin@123.no>
2013-04-19 13:50:12 +0200

    Implement Interfaces and methods to Person

--- src/no/bluebit/pojo/Person.java
+++ src/no/bluebit/pojo/Person.java
@@ -1,10 +1,10 @@
 package no.bluebit.pojo;
 
 /**
- * Describes a person object in the system.
- *
+ * 	Describes a person object in the system.
+ *	Implements to interfaces
  */
-public class Person {
+public class Person implements IPerson, Comparable<Person> {
 	
 	private String firstname, lastname;
 	private int age;
@@ -43,17 +43,47 @@ public class Person {
 		return age;
 	}
 	
-	/**
-	 *
-	 * @return The full name of the Person
-	 */
-	public String getName() {
-		return getFirstname() + " " + getLastname();
-	}
-	
 	@Override
 	public String toString() {
 		return "Name: " + getName() +"\nAge: " + getAge();
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		//check if the object coming in is the same as "this"
+		if(obj == this) {
+			return true;
+		}
+		
+		//check that the object is a Person
+		if(!(obj instanceof Person)) {
+			return false;
+		}
+		
+		//the object is now a person and not the same
+		Person p = (Person) obj;
+		
+		//compare key values - name and age
+		if(this.age == p.age && this.getName().equals(p.getName())) {
+			return true;
+		}
+		
+		//the object is not the same
+		return false;
+	}
+	
+	/**
+	 * @return The full name of the Person
+	 */
+	@Override
+	public String getName() {
+		return getFirstname() + " " + getLastname();
+		
+	}
 
+	@Override
+	public int compareTo(Person o) {
+		//uses String's compareTo method based on the last name
+		return this.lastname.compareTo(o.lastname);
+	}
 }
